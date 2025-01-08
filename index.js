7f27dfc33704332260c6e3593873e49d21526d31
NNmaki <92058983+NNmaki@users.noreply.github.com>
2025-01-08 16:33:34 +0200

    Updated changes from laptop

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 
-let firstCard = 7
-let secondCard = 14
+// Tama on lapparilla tehty js, jossa getrandomcard fuctio
+
+// githubissa firscard ja seconcard funktiossa myos hardcoded value
+
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let sum = firstCard + secondCard
+
+cards = [firstCard, secondCard]
 
 let hasBlackjack = false
 let isAlive = true
@@ -9,32 +15,41 @@ let isAlive = true
 let message = ""
 
 let messageEl = document.getElementById("message-el")
+let cardsEl = document.getElementById("cards-el")
+let sumEl = document.getElementById("sum-el")
 
-function startGame() {
+function getRandomCard() {
+    return 5
+}
+
+function renderGame() {
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
-        
     } else if (sum === 21) {
         message = "Wohoo! You've got Blackjack!"
         hasBlackjack = true
-        
     } else {
         message = "You're out of the game!"
-        isAlive = false
-        
+        isAlive = false     
     }
-
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++ ) {
+        cardsEl.textContent += cards[i] + " "
+    }
     messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
 }
 
+function startGame() {
+    renderGame()
+}
 
-
-
-
-
-
-
-
+function newCard() {
+    let card = getRandomCard()
+    cards.push(card)
+    sum += card
+    renderGame()
+}
 
 
 // console.log(hasBlackjack)
